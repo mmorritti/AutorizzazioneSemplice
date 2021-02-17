58c1e733ad7da86a426c124fcdf4615e482acd89
Mattia Morritti <mmorritti@welol.it>
2021-02-17 17:54:44 +0100

    .

--- AutorizzazioneSemplice/Controllers/UserController.cs
+++ AutorizzazioneSemplice/Controllers/UserController.cs
@@ -1,5 +1,6 @@
 ﻿using AutorizzazioneSemplice.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -30,6 +31,26 @@ namespace AutorizzazioneSemplice.Controllers
             {
                 throw ex;
             }
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "Administrator")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Create(IFormCollection form)
+        {
+            _ctx.Users.Add(new Models.User()
+            {
+                name = form["name"]
+            }) ;
+            _ctx.SaveChanges();
+            return RedirectToAction("Index");
+
         }
     }
 }
